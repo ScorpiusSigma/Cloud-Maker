d5bae212d610d3fd38f26fb51258458816174e84
Justin <justinleng_@hotmail.com>
2022-08-05 21:06:04 +0800

    [Edit] Hide "Drag the corner to resize me"

--- components/ImgProxy.js
+++ components/ImgProxy.js
@@ -4,8 +4,8 @@ import FrameConfig from "./FrameConfig";
 export default function ImgProxy() {
   return (
     <div className="relative select-none flex justify-center items-center w-full max-w-7xl h-full mx-20">
-      <div className="absolute border-t-2 border-l-2 top-0 left-0 w-5 h-5 -translate-x-4 -translate-y-4 hidden md:flex">
-        <span className="absolute text-md w-[70px] text-[16px] leading-none right-0 -translate-y-full text-right py-2">
+      <div className="absolute border-t-2 border-l-2 top-0 left-0 w-5 h-5 -translate-x-4 -translate-y-4">
+        <span className="absolute text-md w-[70px] text-[16px] leading-none right-0 -translate-y-full text-right py-2 hidden 2xl:flex">
           Drag the corner to resize me
         </span>
       </div>
